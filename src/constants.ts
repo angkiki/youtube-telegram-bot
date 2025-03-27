f6bec683743493063693cad7b917c8da0b147d89
angkiki <gabrielangzw@gmail.com>
2025-03-27 20:49:57 +0800

    update tele id constants to integers

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const VALID_TELEGRAM_IDS = [
-    '414715659', // PPK
-    '156124844', // PPQ
+    414715659, // PPK
+    156124844, // PPQ
 ];
 
 export const ACCEPTED_COMMANDS = ['/download'];
